c03770215e849ae21352fb56e768488b89714898
ajaykumar-shendage <ajayshendage0406@gmail.com>
2023-08-27 03:45:17 +0530

    consecutive ones Problem

---
+++ src/com/striver/arrayProblems/consecutiveOnes.java
@@ -0,0 +1,31 @@
+package com.striver.arrayProblems;
+
+import java.util.Scanner;
+
+public class consecutiveOnes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+//        findMax(arr, size);
+        findConsecutiveOnes(arr,size);
+    }
+    private static void findConsecutiveOnes(int[] arr, int n) {
+        int cnt = 0;
+        int maxi = 0;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == 1) {
+                cnt++;
+                maxi = Math.max(maxi,cnt);
+            }
+            else {
+                cnt = 0;
+            }
+        }
+        System.out.println(maxi);
+    }
+}
