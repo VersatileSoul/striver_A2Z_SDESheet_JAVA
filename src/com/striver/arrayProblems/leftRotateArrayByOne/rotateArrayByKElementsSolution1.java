96381cd0b48ea210a00d4b42e1cb8bf27fb2e683
ajaykumar-shendage <ajayshendage0406@gmail.com>
2023-08-30 17:42:26 +0530

    Left Rotate Array by K elements Problem added

---
+++ src/com/striver/arrayProblems/leftRotateArrayByOne/rotateArrayByKElementsSolution1.java
@@ -0,0 +1,29 @@
+package com.striver.arrayProblems;
+import java.util.*;
+public class rotateArrayByKElements1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        k = k % size;
+        int[] ans;
+        ans = rotate(arr, size,k);
+        for (int it : ans) {
+            System.out.print(it + " ");
+        }
+    }
+    private static int[] rotate(int[] arr, int n,int k) {
+        if( k == 0 ) return arr;
+        int temp = arr[0];
+        for(int i = 1; i < n; i++) {
+            arr[i - 1] = arr[i];
+        }
+        arr[n - 1] = temp;
+        rotate(arr,n,k-1);
+        return arr;
+    }
+}
